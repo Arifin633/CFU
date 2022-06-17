91050085b5a98f173ad574a8ce7fa3f108729c56
Anonymous <ADDRESS@HIDDEN>
2022-06-17 10:24:28 -0300

    Add Windmills
    * Items/Garden/Windmill.cs:
    * Textures/Items/Garden/Windmill.png:
    * Textures/Tiles/Garden/Windmill.png:
    * Tiles/Garden/Windmill.cs: New files.

---
+++ Tiles/Garden/Windmill.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Enums;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Terraria.DataStructures;
+
+namespace CFU.Tiles
+{
+    public class Windmill : ModTile
+    {
+        public override string Texture => "CFU/Textures/Tiles/Garden/Windmill";
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 20 };
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.Direction = TileObjectDirection.PlaceRight;
+            TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
+            TileObjectData.newAlternate.Direction = TileObjectDirection.PlaceLeft;
+            TileObjectData.addAlternate(1);
+            TileObjectData.addTile(Type);
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Windmill");
+            AddMapEntry(new Color(191, 142, 111), name);
+            AnimationFrameHeight = 40;
+        }
+
+        public override void AnimateTile(ref int frame, ref int frameCounter)
+        {
+            if (++frameCounter >= 6)
+            {
+                frameCounter = 0;
+                frame = ++frame % 3;
+            }
+        }
+
+        public override void SetDrawPositions(int i, int j, ref int width, ref int offsetY, ref int height, ref short tileFrameX, ref short tileFrameY)
+    => offsetY = -2;
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 16, 32, ModContent.ItemType<Items.Windmill>());
+        }
+    }
+}
