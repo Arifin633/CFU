bb75cb97250f3d47eb0d3de8a12680d61d3600c8
Anonymous <ADDRESS@HIDDEN>
2022-06-28 15:34:57 -0300

    Make Black Ink place-able
    * CFUVanillaOverrides.cs (SetDefaults):
    Make the vanilla Black Ink item place the Ink tile.
    
    * TODO (Ink): Remove done entry.

--- CFUVanillaOverrides.cs
+++ CFUVanillaOverrides.cs
@@ -10,7 +10,19 @@ namespace ChadsFurnitureUpdated
         public override void SetDefaults(Item Item)
         {
             if (Item.type == ItemID.Keg)
+            {
                 Item.createTile = ModContent.TileType<Tiles.Keg>();
+            }
+            else if (Item.type == ItemID.BlackInk)
+            {
+                Item.useTurn = true;
+                Item.autoReuse = true;
+                Item.useAnimation = 15;
+                Item.useTime = 10;
+                Item.useStyle = ItemUseStyleID.Swing;
+                Item.consumable = true;
+                Item.createTile = ModContent.TileType<Tiles.Ink>();
+            }
         }
     }
 }
