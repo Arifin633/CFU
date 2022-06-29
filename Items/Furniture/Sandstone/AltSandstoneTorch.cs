c0911b645fe261cc2e01f3b82442bf19ce1193c7
Anonymous <ADDRESS@HIDDEN>
2022-06-29 19:04:47 -0300

    Fix the Sandstone Torch not having the correct effects when held
    * Items/Furniture/Sandstone/AltSandstoneTorch.cs:
    (AutoStaticDefaults): Fix the flame texture needing an underscore.
    (SetStaticDefaults): Draw flame.
    (HoldItem, PostUpdate): Uncomment.
    
    * Textures/Items/Furniture/Sandstone/AltSandstoneTorchFlame.png: New file.

--- Items/Furniture/Sandstone/AltSandstoneTorch.cs
+++ Items/Furniture/Sandstone/AltSandstoneTorch.cs
@@ -1,14 +1,28 @@
-// using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
 using Terraria;
 using Terraria.ID;
+using Terraria.GameContent;
 using Terraria.ModLoader;
 using Terraria.GameContent.Creative;
 
 namespace CFU.Items
 {
+
     public class AltSandstoneTorch : ModItem
     {
+
         public override string Texture => "CFU/Textures/Items/Furniture/Sandstone/AltSandstoneTorch";
+
+        public override void AutoStaticDefaults()
+        {
+            TextureAssets.Item[this.Item.type] = ModContent.Request<Texture2D>(this.Texture, (AssetRequestMode)2);
+            if (ModContent.RequestIfExists<Texture2D>(this.Texture + "Flame", out var flameTexture, (AssetRequestMode)2))
+            {
+                TextureAssets.ItemFlame[this.Item.type] = flameTexture;
+            }
+        }
 
         public override void SetStaticDefaults()
         {
@@ -18,6 +32,7 @@ namespace CFU.Items
 
         public override void SetDefaults()
         {
+            Item.flame = true;
             Item.width = 10;
             Item.height = 12;
             Item.maxStack = 99;
@@ -38,16 +53,8 @@ namespace CFU.Items
             itemGroup = ContentSamples.CreativeHelper.ItemGroup.Torches;
         }
 
-
-        /* All of this works way too badly and is not really necessary.
-           TODO: Maybe figure this out later.
-        /* public override void HoldItem(Player player)
+        public override void HoldItem(Player player)
         {
-            if (Main.rand.NextBool(player.itemAnimation > 0 ? 40 : 80))
-            {
-                Dust.NewDust(new Vector2(player.itemLocation.X + 16f * player.direction, player.itemLocation.Y - 14f * player.gravDir), 4, 4, DustID.DesertTorch, 0, 0, 0, new Color(1f, 0.5f, 0f));
-            }
-
             Vector2 position = player.RotatedRelativePoint(new Vector2(player.itemLocation.X + 12f * player.direction + player.velocity.X, player.itemLocation.Y - 14f + player.velocity.Y), true);
             Lighting.AddLight(position, 1f, 0.5f, 0f);
         }
@@ -58,7 +65,7 @@ namespace CFU.Items
             {
                 Lighting.AddLight(Item.Center, 1f, 0.5f, 0f);
             }
-        } */
+        }
 
         public override void AutoLightSelect(ref bool dryTorch, ref bool wetTorch, ref bool glowstick)
         {
