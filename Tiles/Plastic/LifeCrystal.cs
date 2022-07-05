63271be024770d103f5528dd61aed4c682600352
Anonymous <ADDRESS@HIDDEN>
2022-07-05 01:14:08 -0300

    Add a Decorative Life Crystal
    * Items/Plastic/LifeCrystal.cs:
    * Textures/Items/Plastic/LifeCrystal.png:
    * Textures/Tiles/Plastic/LifeCrystal.png:
    * Tiles/Plastic/LifeCrystal.cs: New files.

---
+++ Tiles/Plastic/LifeCrystal.cs
@@ -0,0 +1,41 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Terraria.DataStructures;
+
+namespace CFU.Tiles
+{
+    public class LifeCrystal : ModTile
+    {
+        public override string Texture => "CFU/Textures/Tiles/Plastic/LifeCrystal";
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16 };
+            TileObjectData.addTile(Type);
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Life Crystal");
+            AddMapEntry(new Color(174, 24, 69), name);
+            HitSound = SoundID.Shatter;
+            DustType = DustID.HeartCrystal;
+            AnimationFrameHeight = 36;
+        }
+		
+        public override void AnimateTile(ref int frame, ref int frameCounter)
+        {
+            if (++frameCounter >= 6)
+            {
+                frameCounter = 0;
+                frame = ++frame % 10;
+            }
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 16, 32, ModContent.ItemType<Items.LifeCrystal>());
+        }
+    }
+}
