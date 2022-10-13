ebdf5df3fdb876c08908b3174fac1462a3316e35
Anonymous <ADDRESS@HIDDEN>
2022-10-13 11:25:29 -0300

    Add Abigail's Flower and Glow Tulip
    * TODO.UPDATE (Miracle Plants): Delete item.
    
    * Items/Miracle Plants/MiracleAbigailsFlower.cs:
    * Items/Miracle Plants/MiracleGlowTulip.cs:
    * Textures/Items/Miracle Plants/MiracleAbigailsFlower.png:
    * Textures/Items/Miracle Plants/MiracleGlowTulip.png:
    * Textures/Tiles/Miracle Plants/MiracleAbigailsFlower.png:
    * Textures/Tiles/Miracle Plants/MiracleGlowTulip.png:
    * Tiles/Miracle Plants/MiracleAbigailsFlower.cs:
    * Tiles/Miracle Plants/MiracleGlowTulip.cs: New files.

---
+++ Tiles/Miracle Plants/MiracleAbigailsFlower.cs
@@ -0,0 +1,42 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.Enums;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Terraria.DataStructures;
+
+namespace CFU.Tiles
+{
+    public class MiracleAbigailsFlower : ModTile
+    {
+        public override string Texture => "CFU/Textures/Tiles/Miracle Plants/MiracleAbigailsFlower";
+
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            TileID.Sets.SwaysInWindBasic[Type] = true;
+            TileID.Sets.DisableSmartCursor[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
+            TileObjectData.newTile.AnchorAlternateTiles = new int[] { TileID.ClayPot };
+            TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.AlternateTile | AnchorType.SolidWithTop | AnchorType.SolidSide, TileObjectData.newTile.Width, 0);
+            TileObjectData.newTile.CoordinateHeights = new int[] { 18 };
+            TileObjectData.newTile.CoordinateWidth = 20;
+            TileObjectData.newTile.RandomStyleRange = 2;
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.addTile(Type);
+            ModTranslation name = CreateMapEntryName();
+            AddMapEntry(new Color(210, 91, 77));
+            HitSound = SoundID.Grass;
+            DustType = -1;
+        }
+
+        public override bool Drop(int i, int j)
+        {
+            Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 32, 48, ModContent.ItemType<Items.MiracleAbigailsFlower>());
+            return true;
+        }
+    }
+}
---
+++ Tiles/Miracle Plants/MiracleGlowTulip.cs
@@ -0,0 +1,47 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.Enums;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Terraria.DataStructures;
+
+namespace CFU.Tiles
+{
+    public class MiracleGlowTulip : ModTile
+    {
+        public override string Texture => "CFU/Textures/Tiles/Miracle Plants/MiracleGlowTulip";
+
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            TileID.Sets.SwaysInWindBasic[Type] = true;
+            TileID.Sets.DisableSmartCursor[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
+            TileObjectData.newTile.AnchorAlternateTiles = new int[] { TileID.ClayPot };
+            TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.AlternateTile | AnchorType.SolidWithTop | AnchorType.SolidSide, TileObjectData.newTile.Width, 0);
+            TileObjectData.newTile.CoordinateHeights = new int[] { 34 };
+            TileObjectData.newTile.CoordinateWidth = 24;
+            TileObjectData.newTile.RandomStyleRange = 2;
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.addTile(Type);
+            ModTranslation name = CreateMapEntryName();
+            AddMapEntry(new Color(21, 124, 212));
+            HitSound = SoundID.Grass;
+            DustType = -1;
+        }
+
+        public override void SetDrawPositions(int i, int j, ref int width, ref int offsetY, ref int height, ref short tileFrameX, ref short tileFrameY)
+        {
+            offsetY -= 14;
+        }
+
+        public override bool Drop(int i, int j)
+        {
+            Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 32, 48, ModContent.ItemType<Items.MiracleGlowTulip>());
+            return true;
+        }
+    }
+}
