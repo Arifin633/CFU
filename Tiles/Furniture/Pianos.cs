b80fbb3b47407486525253c0f5bdc3abad28e820
Anonymous <ADDRESS@HIDDEN>
2022-10-28 05:01:34 -0300

    * Tiles/Furniture/Pianos.cs: Fix placement

--- Tiles/Furniture/Pianos.cs
+++ Tiles/Furniture/Pianos.cs
@@ -14,6 +14,7 @@ namespace CFU.Tiles
             Main.tileFrameImportant[Type] = true;
             TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2);
             TileObjectData.newTile.Origin = new Point16(0, 1);
+            TileObjectData.newTile.StyleHorizontal = false;
             TileObjectData.newTile.CoordinateHeights = new int[] { 16, 18 };
             TileObjectData.addTile(Type);
             ModTranslation name = CreateMapEntryName();
