847c1a264b45d7fc2e82c9836981407a3608d729
Anonymous <ADDRESS@HIDDEN>
2022-12-04 22:47:44 -0300

    Add Rubblemaker 2.0
    * Items/Rubblemaker/RubblemakerPileLarge.cs:
    * Items/Rubblemaker/RubblemakerPileMedium.cs:
    * Items/Rubblemaker/RubblemakerPilePot.cs:
    * Items/Rubblemaker/RubblemakerPileSmall.cs:
    * Items/Rubblemaker/RubblemakerPileStalactiteMedium.cs:
    * Items/Rubblemaker/RubblemakerPileStalactiteSmall.cs:
    * Items/Rubblemaker/RubblemakerPlaceholder.cs:
    * Textures/Items/Rubblemaker/RubblemakerPileLarge.png:
    * Textures/Items/Rubblemaker/RubblemakerPileMedium.png:
    * Textures/Items/Rubblemaker/RubblemakerPileSmall.png:
    * Textures/Items/Rubblemaker/RubblemakerPlaceholder.png:
    * Textures/Items/Rubblemaker/RubblemakerPot.png:
    * Textures/Items/Rubblemaker/RubblemakerStalactiteMedium.png:
    * Textures/Items/Rubblemaker/RubblemakerStalactiteSmall.png: New files.

---
+++ Items/Rubblemaker/RubblemakerPileStalactiteMedium.cs
@@ -0,0 +1,65 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.Creative;
+
+namespace CFU.Items
+{
+    public class RubblemakerStalactiteMedium : ModItem
+    {
+        public override string Texture => "CFU/Textures/Items/Rubblemaker/RubblemakerStalactiteMedium";
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Rubblemaker 2.0 (Stalactites & Stalagmites)");
+            Tooltip.SetDefault("Used with materials to place matching piles, stalactites, stalagmites, and pots\n<right> to toggle placement type and size\nPress Up/Down to cycle through styles\n'Not a piledriver nor a placeinator: It's a Rubblemaker'");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 16;
+            Item.height = 22;
+            Item.maxStack = 1;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = false;
+            Item.value = 0;
+            Item.placeStyle = 0;
+        }
+
+        public override void AddRecipes()
+        {
+            /* TODO: Uncomment in 1.4.4
+            CreateRecipe()
+            .AddIngredient(ItemID.RubbleMakerSmall)
+            .AddIngredient(ItemID.MythrilBar, 5)
+            .AddTile(TileID.MythrilAnvil)
+            .Register();
+
+            CreateRecipe()
+            .AddIngredient(ItemID.RubbleMakerMedium)
+            .AddIngredient(ItemID.MythrilBar, 5)
+            .AddTile(TileID.MythrilAnvil)
+            .Register();
+
+            CreateRecipe()
+            .AddIngredient(ItemID.RubbleMakerLarge)
+            .AddIngredient(ItemID.MythrilBar, 5)
+            .AddTile(TileID.MythrilAnvil)
+            .Register();*/
+        }
+
+        public override bool CanRightClick() => true;
+
+        public override bool ConsumeItem(Player player) => false;
+
+        public override void RightClick(Player player)
+        {
+            this.Item.SetDefaults(ModContent.ItemType<Items.RubblemakerPot>());
+            this.Item.stack = 1;
+        }
+    }
+}
